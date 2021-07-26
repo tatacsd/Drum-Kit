a29230ad5d818141c0902138169c9f512bad6840
Thays Casado <thayscasado@hotmail.com>
2021-07-26 01:24:28 -0700

    added a general sound when clicked

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", () => {
-    alert("clicked");
+    let audio = new Audio("sounds/crash.mp3");
+    audio.play();
   });
 }
